c49cd47af4ac3b670da93847cd1dbc973bd63039
chriskoder <gerokristof0909@gmail.com>
2020-01-02 20:42:16 +0100

    made changes in the script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let currentOperator, numberOne, numberTwo, switched, values;
 const buttons = document.querySelector('.buttons');
+const operationArray = document.querySelectorAll('.operator');
 const numberArray = document.querySelectorAll('.number');
 const OPERATION_SELECTORS = {
     btnDivide: document.querySelector('.slash'),
@@ -17,38 +18,64 @@ const DISPLAY_SELECTORS = {
     dispOutcome: document.querySelector('.outcome')
 }
 let init = (function () {
+    clear();
     currentOperator = '';
     numberOne = '';
     numberTwo = '';
     switched = false;
     numberArr = Array.from(numberArray); //since numberArray is a nodelist
+    operationArr = Array.from(operationArray);
     buttons.onclick = function(event) {
         let target = event.target;
         let inside = target.classList.contains('button-inside');
         if (inside) {
             target = event.target.parentElement;
-            let num = target.classList.contains("number");
-            let n = target.getAttribute('data-value');
-            if (num) {
-                if (switched) {
-                    numberOne += n;
+            if(target.classList.contains('number')) {
+                if(switched) {
+                    numberTwo += target.getAttribute('data-value');
+                    update(numberTwo)
                 } else {
-                    numberTwo += n;
+                    numberOne += target.getAttribute('data-value');
+                    update(numberOne)
                 }
+            } else if(target.classList.contains('operator')) {
+                checkButtonPress(target);
             }
         } else {
-            let num = target.classList.contains('number');
-            if(num) {
-                if (switched) {
-                    numberOne += n;
+            if(target.classList.contains('number')) {
+                if(switched) {
+                    numberTwo += target.getAttribute('data-value');
+                    update(numberTwo)
                 } else {
-                    numberTwo += n;
+                    numberOne += target.getAttribute('data-value');
+                    update(numberOne)
                 }
+            } else if(target.classList.contains('operator')) {
+                checkButtonPress(target);
             }
         }
     }
     OPERATION_SELECTORS.btnEqual.addEventListener('click', operate);
+    
 })();
 function operate(op, a, b) {
     console.log('should operate');
+}
+function update(x) {
+    DISPLAY_SELECTORS.dispCurrent.textContent = x;
+}
+function clear() {
+    numberOne = '';
+    numberTwo = '';
+    DISPLAY_SELECTORS.dispCurrent.textContent = '0';
+    DISPLAY_SELECTORS.dispOutcome.textContent = '0';
+    switched = false;
+}
+function checkButtonPress(trg) {
+    if(trg.getAttribute('data-value') === 'clear') {
+        clear();
+    } else if(trg.getAttribute('data-value') === 'multiply') {
+        str = numberOne;
+        switched = true;
+    }
 }
