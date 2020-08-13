de904ebb212271df46376f5221a0c09c21e6d23c
chriskoder <gerokristof0909@gmail.com>
2020-08-13 18:06:52 +0200

    Rewrote the JS logic, fixed every bug

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let currentOperator, numberOne, numberTwo, switched, values, done;
+let currentOperator, currentOperatorValue, numberOne, numberTwo, switched, e;
 const buttons = document.querySelector('.buttons');
 const operationArray = document.querySelectorAll('.operator');
 const numberArray = document.querySelectorAll('.number');
@@ -8,7 +8,7 @@ const OPERATION_SELECTORS = {
     btnSubstract: document.querySelector('.substract'),
     btnEqual: document.querySelector('.equal'),
     btnAdd: document.querySelector('.add'),
-    btnSqrt: document.querySelector('.sqrt'),
+    btnPow: document.querySelector('.pow'),
     btnClear: document.querySelector('.clear'),
     btnRemainder: document.querySelector('.rem'),
     btnMultiply: document.querySelector('.multiply')
@@ -18,198 +18,173 @@ const DISPLAY_SELECTORS = {
     dispCurrent: document.querySelector('.numbers'),
     dispOutcome: document.querySelector('.outcome')
 }
-let init = (function () {
-    clear();
-    done = 0;
-    currentOperator = '';
+allClear = () => {
     numberOne = '';
     numberTwo = '';
+    currentOperator = '';
+    e = '';
     switched = false;
-    numberArr = Array.from(numberArray); //since numberArray is a nodelist
-    operationArr = Array.from(operationArray);
+    DISPLAY_SELECTORS.dispPrevious.textContent = '0';
+    DISPLAY_SELECTORS.dispCurrent.textContent = '0';
+    DISPLAY_SELECTORS.dispOutcome.textContent = '0';
+}
+handleButtonClicks = () => {
     buttons.onclick = function(event) {
         let target = event.target;
         let inside = target.classList.contains('button-inside');
-        if (inside) {
+        if(inside) {
             target = event.target.parentElement;
-            if(target.classList.contains('number')) {
-                if(switched) {
-                    numberTwo += target.getAttribute('data-value');
-                    update(numberTwo)
+            if (target.classList.contains('number')) {
+                if(e === '') {
+                    if(!switched) {
+                        numberOne += target.getAttribute('data-value');
+                    } else {
+                        numberTwo += target.getAttribute('data-value');
+                    }
                 } else {
+                    allClear();
                     numberOne += target.getAttribute('data-value');
-                    update(numberOne)
                 }
-            } else if(target.classList.contains('operator')) {
-                if (done > 0) {
-                    checkButtonPress(target);
-                    DISPLAY_SELECTORS.dispPrevious.textContent = numberOne + currentOperator;
-                } else {
-                    checkButtonPress(target);
+            } else if (target.classList.contains('operator')) {
+                if(target.getAttribute('data-value') != 'clear' && target.getAttribute('data-value') != 'del') {
+                    if(e === '') {
+                        currentOperator = target.getAttribute('data-value');
+                        getOperatorByName();
+                        switched = true;
+                    } else {
+                        if(e != 'Math error') {
+                            currentOperator = target.getAttribute('data-value');
+                            getOperatorByName();
+                            numberOne = e;
+                            e = '';
+                            numberTwo = '';
+                            DISPLAY_SELECTORS.dispOutcome.textContent = '0'; 
+                        }
+                    }
+                }  else if(target.getAttribute('data-value') === 'del') {
+                    deleteLast();
+                } else if (target.getAttribute('data-value') === 'clear') {
+                    allClear();
                 }
             }
         } else {
-            if(target.classList.contains('number')) {
-                if(switched) {
-                    numberTwo += target.getAttribute('data-value');
-                    update(numberTwo)
+            if (target.classList.contains('number')) {
+                if(e == '') {
+                    if(!switched) {
+                        numberOne += target.getAttribute('data-value');
+                    } else {
+                        numberTwo += target.getAttribute('data-value');
+                    }
                 } else {
+                    allClear();
                     numberOne += target.getAttribute('data-value');
-                    update(numberOne)
                 }
-            } else if(target.classList.contains('operator')) {
-                if (done > 0) {
-                    checkButtonPress(target);
-                    console.log(target);
-                    DISPLAY_SELECTORS.dispPrevious.textContent = numberOne + currentOperator;
-                } else {
-                    checkButtonPress(target);
-                }
-            }
-        }
-    }
-    OPERATION_SELECTORS.btnEqual.addEventListener('click', function() {
-        switched = false;
-        if (numberTwo != '') {
-            e = operate(currentOperator, numberOne, numberTwo);
-            console.log(isNaN(e));
-            if (!isNaN(e)) {
-                if(isFinite(e)) {
-                    DISPLAY_SELECTORS.dispOutcome.textContent = e;
-                    numberOne = e;
-                    numberTwo = '';
-                    currentOperator = '';
-                    done++;
-                } else {
-                    DISPLAY_SELECTORS.dispOutcome.textContent = 'Math error';
-                    numberTwo = '';
-                    currentOperator = '';
-                    done++;
-                }
-            } else {
-                DISPLAY_SELECTORS.dispOutcome.textContent = 'Syntax error';
-                numberTwo = '';
-                currentOperator = '';
-                done++;
-            }
-        } else {
-            if(currentOperator === '√') {
-                e = operate(currentOperator, numberOne, numberTwo);
-                if(!isNaN(e)) {
-                    DISPLAY_SELECTORS.dispOutcome.textContent = e;
-                    numberOne = e;
-                    numberTwo = '';
-                    currentOperator = '';
-                    done++;
-                } else {
-                    DISPLAY_SELECTORS.dispOutcome.textContent = 'Math error';
-                    numberOne = '0';
-                    numberTwo = '';
-                    currentOperator = '';
-                    done++;
+            } else if (target.classList.contains('operator')) {
+                if(target.getAttribute('data-value') != 'clear' && target.getAttribute('data-value') != 'del') {
+                    if(e === '') {
+                        currentOperator = target.getAttribute('data-value');
+                        getOperatorByName();
+                        switched = true;
+                    } else {
+                        if(e != 'Math error') {
+                            currentOperator = target.getAttribute('data-value');
+                            getOperatorByName();
+                            numberOne = e;
+                            e = '';
+                            numberTwo = '';
+                            DISPLAY_SELECTORS.dispOutcome.textContent = '0'; 
+                        }
+                    }
+                } else if(target.getAttribute('data-value') === 'del') {
+                    deleteLast();
+                } else if (target.getAttribute('data-value') === 'clear') {
+                    allClear();
                 }
             }
         }
-    });
-    OPERATION_SELECTORS.btnDelete.addEventListener('click', delOne);
-})();
-function delOne(cur) {
-    if(DISPLAY_SELECTORS.dispCurrent.textContent != '0') {
-        if(DISPLAY_SELECTORS.dispOutcome != 'Syntax error') {
-            if(switched) {
-                if(numberTwo.length > 1) {
-                    cur = numberTwo;
-                    numberTwo = numberTwo.slice(0, -1);
-                    update(numberTwo);
-                } else {
-                    numberTwo = '';
-                    update('0');
-                }
-            } else {
-                if(numberOne.length > 1) {
-                    cur = numberOne;
-                    numberOne = numberOne.slice(0, -1);
-                    update(numberOne);
-                    DISPLAY_SELECTORS.dispPrevious.textContent = '0';
-                } else {
-                    numberOne = '0';
-                    update('0');
-                }
-            }
-        }
+        render();
     }
 }
-function operate(op, a, b) {
-    op = currentOperator;
-    a = parseFloat(numberOne);
-    b = parseFloat(numberTwo);
-    if (op === 'x') {
-        return Math.round((a * b)*1000) / 1000;
-    } else if (op === '÷') {
-        return Math.round((a / b)*1000) / 1000;
-    } else if (op === '+') {
-        return Math.round((a + b)*1000) / 1000;
-    } else if (op === '-') {
-        return Math.round((a - b)*1000) / 1000;
-    } else if (op === '%') {
-        return Math.round((a % b)*1000) / 1000;
-    } else if (op === '√') {
-        return Math.round((Math.sqrt(a))*1000) / 1000;
-    }
-}
-function update(x) {
-    DISPLAY_SELECTORS.dispCurrent.textContent = x;
-}
-function clear() {
-    switched = false;
-    numberOne = '0';
-    numberTwo = '';
-    DISPLAY_SELECTORS.dispCurrent.textContent = '0';
-    DISPLAY_SELECTORS.dispOutcome.textContent = '0';
-    DISPLAY_SELECTORS.dispPrevious.textContent = '0';
-}
-function checkButtonPress(trg) {
-    if (numberOne != '') {
+render = () => {
+    if(currentOperator != 'equal') {
         if(!switched) {
-            if(trg.getAttribute('data-value') === 'clear') {
-                clear();
-            } else if(trg.getAttribute('data-value') === 'multiply') {
-                currentOperator = 'x';
-                str = numberOne;
-                switched = true;
-                DISPLAY_SELECTORS.dispPrevious.textContent = numberOne + currentOperator;
-                DISPLAY_SELECTORS.dispCurrent.textContent = '0';
-            } else if(trg.getAttribute('data-value') === 'divide') {
-                currentOperator = '÷';
-                str = numberOne;
-                switched = true;
-                DISPLAY_SELECTORS.dispPrevious.textContent = numberOne + '÷';
-                DISPLAY_SELECTORS.dispCurrent.textContent = '0';
-            } else if(trg.getAttribute('data-value') === 'add') {
-                currentOperator = '+';
-                str = numberOne;
-                switched = true;
-                DISPLAY_SELECTORS.dispPrevious.textContent = numberOne + currentOperator;
-                DISPLAY_SELECTORS.dispCurrent.textContent = '0';
-            } else if(trg.getAttribute('data-value') === 'substract') {
-                currentOperator = '-';
-                str = numberOne;
-                switched = true;
-                DISPLAY_SELECTORS.dispPrevious.textContent = numberOne + currentOperator;
-                DISPLAY_SELECTORS.dispCurrent.textContent = '0';
-            } else if(trg.getAttribute('data-value') === 'rem') {
-                currentOperator = '%';
-                str = numberOne;
-                switched = true;
-                DISPLAY_SELECTORS.dispPrevious.textContent = numberOne + currentOperator;
-                DISPLAY_SELECTORS.dispCurrent.textContent = '0';
-            } else if(trg.getAttribute('data-value') === 'sqrt') {
-                currentOperator = '√';
-                switched = true;
-                str = numberOne;
-                DISPLAY_SELECTORS.dispPrevious.textContent = '√' + numberOne;
-                DISPLAY_SELECTORS.dispCurrent.textContent = '√' + numberOne;
+            if(numberOne == '') {
+                DISPLAY_SELECTORS.dispCurrent.textContent = '0'; 
+                numberOne = '';
+            } else {
+                DISPLAY_SELECTORS.dispCurrent.textContent = numberOne; 
+            
+            }
+        } else {
+            if(numberOne != '') {
+                DISPLAY_SELECTORS.dispPrevious.textContent = numberOne + ' ' + currentOperatorValue;
+
+                if(numberTwo != '') {
+                    DISPLAY_SELECTORS.dispCurrent.textContent = numberTwo;
+                } else {
+                    DISPLAY_SELECTORS.dispCurrent.textContent = '0';
+                }
+            } else {
+                numberOne = '0';
             }
         }
     }
 }
+getOperatorByName = () => {
+    if(currentOperator === 'substract') {
+        currentOperatorValue = '-';
+    } else if (currentOperator === 'add') {
+        currentOperatorValue =  '+';
+    } else if (currentOperator === 'divide') {
+        currentOperatorValue =  '÷';
+    } else if (currentOperator === 'multiply') {
+        currentOperatorValue =  'x';
+    } else if (currentOperator === 'rem') {
+        currentOperatorValue =  '%';
+    } else if (currentOperator === 'pow') {
+        currentOperatorValue =  '^';
+    } else if (currentOperator === 'equal') {
+        DISPLAY_SELECTORS.dispOutcome.textContent = '= ' + operate(numberOne, numberTwo, currentOperatorValue);
+    }
+    DISPLAY_SELECTORS.dispPrevious.textContent = numberOne + ' ' + currentOperatorValue;
+    if(numberOne === '') {
+        DISPLAY_SELECTORS.dispPrevious.textContent = '0' + ' ' + currentOperatorValue;
+    }
+}
+operate = (a, b, op) => {
+    if(DISPLAY_SELECTORS.dispCurrent.textContent === '0') {
+        b = 0;
+    }
+    if(op === '-') {
+        e  = parseFloat(a) - parseFloat(b);
+    } else if (op === '+') {
+        e = parseFloat(a) + parseFloat(b);
+    } else if (op === '÷') {
+        if(b != 0) {
+            e = parseFloat(a) / parseFloat(b);
+        } else {
+            e = 'Math error'
+            return e;
+        }
+    } else if (op === 'x') {
+        e = parseFloat(a) * parseFloat(b);
+    } else if (op === '%') {
+        e = parseFloat(a) % parseFloat(b);
+    } else if (op === '^') {
+        e = Math.pow(parseFloat(a), parseFloat(b));
+    }
+    switched = false;
+    return e.toFixed(2);
+}
+deleteLast = () => {
+    if(!switched) {
+        numberOne = numberOne.slice(0, -1);
+    } else {
+        numberTwo = numberTwo.slice(0, -1);
+    }
+}
+init = (() => {
+    e = '';
+    allClear();
+    handleButtonClicks();
+})();
